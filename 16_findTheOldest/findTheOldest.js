190ba1e5f6058bf784a7c416a6b0efcdc0b1ead7
malcomtyk21 <malcomtyk21@yahoo.com>
2025-07-12 14:23:15 +0800

    16. findTheOldest done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+    oldest = { age : 0}
 
+    for (let i = 0; i < peoples.length; i++) {
+        if (peoples[i].yearOfDeath == undefined) {
+            peoples[i].yearOfDeath = new Date().getFullYear();
+        }
+        age = peoples[i].yearOfDeath - peoples[i].yearOfBirth;
+        if (oldest.age < age) {
+            oldest.name = peoples[i].name
+            oldest.age = age
+        }
+    }
+    return oldest
 };
 
 // Do not edit below this line
