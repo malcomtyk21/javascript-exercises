0bae8ab8d222709fa647493f6412723f626b43ec
malcomtyk21 <malcomtyk21@yahoo.com>
2025-07-12 13:52:15 +0800

    13. palindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    output = ''
+    for (let i = 0; i <= str.length; i++) {
+        if (/^[a-zA-Z()0-9]$/.test(str[i])) {
+            output += str[i]
+        }
+    }
 
+    return output.toLowerCase() == output.toLowerCase().split('').reverse().join('')
 };
 
 // Do not edit below this line
