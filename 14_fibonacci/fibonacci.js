f63f72f89f6ab6bc26fa23c5c5392a4bd9f258c5
malcomtyk21 <malcomtyk21@yahoo.com>
2025-07-12 14:02:31 +0800

    14. fibonacci done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else if (num <= 2) {
+        return 1;
+    }
 
+    seq = [1, 1]
+
+    for (let i = 2; i < num; i++) {
+        seq.push(seq[i-1] + seq[i-2]);
+    }
+
+    return seq[seq.length - 1];
 };
 
 // Do not edit below this line
